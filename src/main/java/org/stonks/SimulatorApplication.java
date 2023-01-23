93dfeceae0d1a191d5e8316ca2cfa5a3d84d7473
kingadamo <78397077+kingadamo@users.noreply.github.com>
2023-01-22 19:32:38 -0500

    Main renamed

--- src/main/java/org/stonks/SimulatorApplication.java
+++ src/main/java/org/stonks/SimulatorApplication.java
@@ -1,18 +1,26 @@
 package org.stonks;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.stonks.simulator.MarketSimulator;
 import org.stonks.simulator.TransactionEventEmitter;
 
-public class Main {
+@SpringBootApplication
+public class SimulatorApplication {
     public static void main(String[] args) throws InterruptedException {
         System.out.println("Starting!");
         Responder responder = new Responder();
-        MarketSimulator marketSimulator = new MarketSimulator();
+
 //        transactionEventSimulator.printBook(5);
         TransactionEventEmitter.addListener(responder);
 //        transactionEventSimulator.tradeOccured();
 
-        marketSimulator.start();
+
+
+        new MarketSimulator();
+
+
+        SpringApplication.run(SimulatorApplication.class, args);
 
     }
 }

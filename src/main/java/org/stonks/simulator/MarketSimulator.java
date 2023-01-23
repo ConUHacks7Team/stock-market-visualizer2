bef7a345ccdfdd3799c1c0921b84e758a5257eb6
kingadamo <78397077+kingadamo@users.noreply.github.com>
2023-01-22 19:32:22 -0500

    Market Simulator now runs in another Thread + records history

--- src/main/java/org/stonks/simulator/MarketSimulator.java
+++ src/main/java/org/stonks/simulator/MarketSimulator.java
@@ -7,13 +7,15 @@ import java.time.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MarketSimulator {
+public class MarketSimulator extends Thread{
 
     public SimulatedClock simulatedClock = new SimulatedClock();
 
-    private TransactionReader transactionReader = new TransactionReader();
-
-    public MarketSimulator(){}
+    public MarketSimulator(){
+        new Thread(() -> {
+            this.start();
+        }).start();
+    }
 
 
     // FOR DEBUG
@@ -21,18 +23,18 @@ public class MarketSimulator {
         System.out.println("Printing!");
 
         for (int i = 0; i < number_of_transactions; i++) {
-            System.out.println(transactionReader.transactionBook.get(i).toString());
+            System.out.println(TransactionReader.transactionBook.get(i).toString());
         }
     }
 
-    public void start() throws InterruptedException {
+    public void start() {
         // TODO: Create internal simulated clock
         Clock simulator_clock = simulatedClock.generateClock();
 
 
-        for (int i = 0; i <  transactionReader.transactionBook.length(); i++) {
+        for (int i = 0; i <  TransactionReader.transactionBook.length(); i++) {
 
-            JSONObject jsonObject = transactionReader.transactionBook.getJSONObject(i);
+            JSONObject jsonObject = TransactionReader.transactionBook.getJSONObject(i);
             String timeStamp = jsonObject.getString("TimeStamp");
             LocalDateTime transactionTime = SimulatedClock.timeStampToLocalDateTime(timeStamp);
 
@@ -42,6 +44,17 @@ public class MarketSimulator {
             //System.out.println("TRANSACTION IN : " + jsonObject.getString("Symbol") + " at " + timeStamp);
             TransactionEventEmitter.onTransactionGeneric( jsonObject );
 
+            // HashMap<String, ArrayList<JSONObject>> repertoryStockTradeHistory
+            String symbol_name = jsonObject.getString("Symbol");
+            ArrayList<JSONObject> stockBook = TransactionReader.repertoryStockTradeHistory.get(symbol_name);
+
+            if (stockBook == null){
+                stockBook = new ArrayList<>();
+            }
+            stockBook.add(jsonObject);
+            TransactionReader.repertoryStockTradeHistory.put(symbol_name, stockBook);
+
+//            transactionReader.repertoryStockTradeHistory.put();
         }
 
 
